6ac12c8995dabcfab3077ee13fef762f850de705
Victor Badenas <victor.badenas@gmail.com>
2020-11-18 22:38:07 +0100

    cleaned a little

--- LAB2/nn.m
+++ LAB2/nn.m
@@ -2,21 +2,15 @@
 clear all; close all;
 
 %% GLOBAL VARIABLES
-HIDDEN_UNITS=50;
-TRAIN_RATIO=0.1;
+HIDDEN_UNITS=200;
+TRAIN_RATIO=0.8;
 TEST_RATIO=0.1;
-VAL_RATIO=0.8;
-EPOCHS=1;
+VAL_RATIO=0.1;
+EPOCHS=2000;
 
 %% LOAD DATA
 load('data/Input.mat');
 load('data/Output.mat');
-% load('data/caltech101_silhouettes_16.mat');
-% load('data/caltech101_silhouettes_16_split1.mat');
-% load('data/caltech101_silhouettes_28.mat');
-% load('data/caltech101_silhouettes_28_split1.mat');
-% Input=Input(:, 1:200);
-% Output=Output(:, 1:200);
 
 %% SIZE OF DATA20
 size(Input)
@@ -24,9 +18,8 @@ size(Output)
 
 %% NET INICIALIZATION
 
-% net=perceptron;
-net=feedforwardnet([20, 2]);
-view(net)
+net=feedforwardnet([HIDDEN_UNITS]);
+
 %% DATA DIVISION
 
 net.divideFcn = 'dividerand';   % divideFCN allow to change the way the data is
@@ -37,37 +30,48 @@ net.divideParam.valRatio = VAL_RATIO;
 
 % Ratio of data used as validation set
 net.divideParam.testRatio = TEST_RATIO; % Ratio of data used as test set
-% net.trainParam.max_fail = 6; % validation check parameter
+net.trainParam.max_fail = 6; % validation check parameter
 net.trainParam.epochs=EPOCHS; % number of epochs parameter
-% net.trainParam.min_grad=1e-5; % minimum performance gradient
+net.trainParam.min_grad=1e-5; % minimum performance gradient
 
 %% LAYERS TYPE DEFINITION
 
-% net.layers{1}.transferFcn = 'logsig';
-% net.layers{1}.transferFcn = 'tansig';
-% net.layers{2}.transferFcn = 'logsig';
-% net.layers{2}.transferFcn = 'softmax';
-% net.performFcn = 'mse';
-% net.performFcn = 'crossentropy';
+net.layers{1}.transferFcn = 'logsig';
+net.layers{2}.transferFcn = 'softmax';
+net.performFcn = 'crossentropy';
+net.trainFcn = 'traingdx';
 
-% net.trainFcn = 'trainlm';    % Levenberg-Marquardt
-% net.trainFcn = 'traingdm';   % Gradient Descent with momentum
-% net.trainFcn = 'traingdx';   % Gradient descent with momentum and adaptive
-%                             % learning rate backpropagation
-
-% net.trainParam.mc = 0.8; % momentum parameter
-% net.trainParam.lr = 0.01; % learning rate parameter
-net.inputs{1}.processFcns = {'mapminmax'};
+net.trainParam.lr = 0.01;	
+net.trainParam.lr_inc = 1.01;	
+net.trainParam.lr_dec = 0.7;
+net.trainParam.max_perf_inc = 1.03;
+net.trainParam.mc = 0.9;
+net.inputs{1}.processFcns = {};
+net.outputs{end}.processFcns = {};
 
 %% TRAIN
 
-% [net,tr,a,b] = train(net,Input,Output);
 [net,tr,Y,E] = train(net,Input,Output,'useGPU','yes');
+performances = [tr.best_perf tr.best_vperf tr.best_tperf]
 
-% net = configure(net,Input,Output);
-% Xgpu = gpuArray(Input);
-% Tgpu = gpuArray(Output);
-% net = train(net,Xgpu,Tgpu);
-% Ygpu = net(Xgpu);
-% Y = gather(Ygpu); 
-view(net)
+%% VALIDATION
+
+[~, Yargmax] = max(Y, [], 1);
+[~, Oargmax] = max(Output, [], 1);
+accVector = Yargmax == Oargmax;
+
+[~, trainMask] = find(tr.trainMask{1} == 1);
+[~, valMask] = find(tr.valMask{1} == 1);
+[~, testMask] = find(tr.testMask{1} == 1);
+
+trainAcc = sum(accVector(trainMask)) / length(trainMask);
+valAcc = sum(accVector(valMask)) / length(valMask);
+testAcc = sum(accVector(testMask)) / length(testMask);
+
+validation = [trainAcc, valAcc, testAcc]
+
+%% PLOT
+
+plot(tr.epoch, tr.perf, tr.epoch, tr.vperf, tr.epoch, tr.tperf)
+legend("train", "validation", "test")
+title("Performance")
